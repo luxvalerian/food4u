411b8cfcfe7875990ac8003574c44e099851e83c
Vincent Robles <sylkisgsx@gmail.com>
2020-04-13 01:07:21 -0400

    added paths for stores, logout, login, and signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('stores/', views.stores, name='stores'),
+    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
