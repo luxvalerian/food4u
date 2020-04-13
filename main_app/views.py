117e5bb9d9420bb78d78e48f695340a0aeeb1f6b
Vincent Robles <sylkisgsx@gmail.com>
2020-04-13 01:07:50 -0400

    defined boilerplate views

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,27 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def stores(request):
+    return render(request, 'stores/index.html')
+
+
+def logout(request):
+    return HttpResponse('Logged Out')
+
+
+def stores_index(request):
+    return render(request, 'stores/index.html')
+
+
+def stores_detail(request):
+    return render(request, 'stores/detail.html')
+
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+
+def signup(request):
+    return render(request, 'registration/signup.html')
