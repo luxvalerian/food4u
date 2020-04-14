dd73da1f73a3634a8e2c9f1f4ca9e246eccf7dd5
William Aleshire <Mine@Williams-iMac.domain>
2020-04-14 12:49:32 -0700

    started user model, commented out

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+# from django.db import models
+# from django.urls import reverse
 
-# Create your models here.
+
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100)
