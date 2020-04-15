8eb6669103d365a61eebabb38a5c235cf1166eb2
William Aleshire <Mine@Williams-iMac.domain>
2020-04-14 17:54:16 -0700

    got form to show

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 from . scrapper import produce_dict, logo_img, logo_svg
 
@@ -33,4 +35,15 @@ def login(request):
 
 
 def signup(request):
-    return render(request, 'registration/signup.html')
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
