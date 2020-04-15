357dc83e5b170fee0a3fecac13c81a31483d0f51
Galata Tona <tonagalata+2@gmail.com>
2020-04-15 01:50:39 -0400

    updates urls list

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('checkout/<int:total_volunteers>/<int:total_checkouts>/', views.checkout, name='checkout'),
+    path('remove/', views.remove_vol, name='remove'),
 ]
