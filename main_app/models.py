8096eab9b5c4b8c6e0d5c27c500f6e1d1ad6d446
William Aleshire <Mine@Williams-iMac.domain>
2020-04-14 18:50:45 -0700

    no comments at this time

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,3 @@
 # from django.db import models
 # from django.urls import reverse
-
-
-# class User(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.CharField(max_length=100)
+# from django.contrib.auth.models import User
