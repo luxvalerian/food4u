3df6c40b95548c8ef6bdb96072fdd57cfccf4f8a
Galata Tona <tonagalata+2@gmail.com>
2020-04-16 19:38:29 -0400

    updates admin registration

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Item, Cart, Volunteer, Customer, User
+from .models import Item, Cart, Volunteer, Customer, User, Timeslot
 
 admin.site.register(User)
 admin.site.register(Volunteer)
 admin.site.register(Customer)
 admin.site.register(Item)
 admin.site.register(Cart)
+admin.site.register(Timeslot)
