dfacd21f611e220e9318736046a4339e187e74ab
Galata Tona <tonagalata+2@gmail.com>
2020-04-16 00:11:58 -0400

    registers cusomer and user models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Item, Cart, Volunteer
+from .models import Item, Cart, Volunteer, Customer, User
 
+admin.site.register(User)
 admin.site.register(Volunteer)
+admin.site.register(Customer)
 admin.site.register(Item)
 admin.site.register(Cart)
