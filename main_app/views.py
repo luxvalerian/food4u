aa47ea2da7b7f03e2bf4437c6639346b5bfa3d4f
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-04-15 19:34:27 -0700

    updated signup redirect

--- main_app/views.py
+++ main_app/views.py
@@ -42,7 +42,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('stores')
         else:
             error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
