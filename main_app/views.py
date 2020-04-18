28be57ed67774969be6041fd90e1a36a30233184
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-04-18 11:26:31 -0700

    pylint changed the spacing

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from .scraper import produce_dict, logo_img, logo_svg
-from .models import Item, Cart, Customer, User, Timeslot #,Volunteer
+from .models import Item, Cart, Customer, User, Timeslot  # ,Volunteer
 from .forms import CustomerSignUpForm, VolunteerSignUpForm
 
 
@@ -29,8 +29,8 @@ def customer_signup(request):
         form = CustomerSignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaded_data.get('username')
-            password = form.cleaded_data.get('password1')
+            username = form.cleared_data.get('username')
+            password = form.cleared_data.get('password1')
             # email= form.cleaded_data.get('email')
             # first_name = form.cleaded_data.get('first_name')
             # last_name = form.cleaded_data.get('last_name')
@@ -42,7 +42,6 @@ def customer_signup(request):
     form = CustomerSignUpForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup_form.html', context)
-
 
 
 class VolunteerSignUpView(CreateView):
@@ -58,6 +57,7 @@ class VolunteerSignUpView(CreateView):
         user = form.save()
         login(self.request, user)
         return redirect('')
+
 
 def home(request):
     return render(request, 'home.html')
@@ -82,6 +82,7 @@ def stores_index(request):
 def stores_detail(request):
     return render(request, 'stores/detail.html')
 
+
 def logout(request):
     return render(request, 'stores/detail.html')
 
@@ -103,24 +104,27 @@ def signup(request):
 
 def remove_vol(request):
     # Timeslot.objects.get(id=timeslot_id).volunteers.remove(volunteer_id)
-    
+
     return redirect('customer/index.html')
 
+
 @login_required
-def checkout(request): #volunteer_id
+def checkout(request):  # volunteer_id
     # volunteer = Volunteer.objects.get(id=volunteer_id)
     # customer = Customer.objects.get(id=customer_id)
     # timeslot = Customer.objects.get(id=timeslot_id)
     # cart = Cart.objects.get(id=cart_id)
-    
+
     # context = { "customer": customer, "timeslot": timeslot, "cart": cart}#"volunteer": volunteer
     return render(request, 'checkout.html')
+
 
 def customer_index(request):
     # customer = Customer.objects.get(id=customer_id)
 
     context = {'customer': customer}
     return render(request, 'customer/index.html')
+
 
 @login_required
 def cart(request):
