90b19e391276787bbc68177e2e9db7330bc8ffb2
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-04-22 11:37:08 -0700

    made new paths for index and detail pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
-    path('stores/', views.stores, name='stores'),
+    path('stores/', views.stores, name='index'),
+    path('stores/detail/', views.stores_detail, name='detail'),
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
     # path('login/volunteer/',
@@ -14,8 +15,10 @@ urlpatterns = [
     path('customer/<int:customer_id>/', views.customer_index, name='customer'),
     path('remove/', views.remove_vol, name='remove'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/signup/volunteer', views.volunteer_signup, name='volunteer_signup'),
+    path('accounts/signup/volunteer',
+         views.volunteer_signup, name='volunteer_signup'),
     path('cart/<int:profile_id>', views.cart, name='cart'),
-    path('customer/<int:pk>/update/', views.CustomerDeliveryTimeUpdate.as_view(), name="customer_time_update"),
+    path('customer/<int:pk>/update/',
+         views.CustomerDeliveryTimeUpdate.as_view(), name="customer_time_update"),
     # path('checkout/<int:profile_id>', views.cart, name='add_timeslot')
 ]
