64894a103d9dd4441ec360939941d9db06e98677
Vincent Robles <sylkisgsx@gmail.com>
2020-04-22 18:54:19 -0400

    adjusted home

--- main_app/views.py
+++ main_app/views.py
@@ -68,10 +68,8 @@ def volunteer_signup(request):
 def home(request):
     volunteer = Volunteer.objects.all()
     context = {'volunteer': volunteer}
-    if volunteer.filter(id=request.user.id):
-        return render(request, 'home.html', context)
-    else:
-        return render(request, 'about.html', context)
+    return render(request, 'home.html', context)
+
 
 
 def about(request):
